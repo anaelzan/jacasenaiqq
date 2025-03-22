265034814025ec2c6144069a0259939195030e7c
anaelzan <anaelzanunespedro@gmail.com>
2025-03-22 10:14:00 -0300

    javasenaiq

---
+++ Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String[] args) {
+
+        int num = 150;
+        long num01 = 10l;
+        byte numNovo = 105;
+
+        String nome = "Ana";
+        System.out.println("meu nome é:"+ nome);
+
+        float num02= 10.5f;
+        double num03 = 10.5;
+                System.out.println(num02 + num03);
+        System.out.println(num02 - num03);
+        System.out.println(num*num03);
+        System.out.println(num/num03);
+
+
+
+    }
+}
